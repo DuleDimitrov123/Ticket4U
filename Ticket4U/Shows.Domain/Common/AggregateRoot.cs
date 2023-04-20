f33c59b69c035dc1cb98a0fb2a5767330966930b
DuleDimitrov123 <65020818+DuleDimitrov123@users.noreply.github.com>
2023-04-20 15:58:03 +0200

    Implement first part of Shows domain

---
+++ Ticket4U/Shows.Domain/Common/AggregateRoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Shows.Domain.Common;
+
+public abstract class AggregateRoot : Entity
+{
+    protected readonly IList<DomainEvent> DomainEvents = new List<DomainEvent>();
+
+    protected void AddEvent(DomainEvent domainEvent)
+    {
+        DomainEvents.Add(domainEvent);
+    }
+
+    //Delete events after saving
+    public void ClearEvents()
+    {
+        DomainEvents.Clear();
+    }
+
+    public ReadOnlyCollection<DomainEvent> GetDomainEvents() => DomainEvents?.AsReadOnly();
+}
---
+++ Ticket4U/Shows.Domain/Shows/Show.cs
@@ -0,0 +1,10 @@
+﻿using Shows.Domain.Common;
+
+namespace Shows.Domain.Shows;
+
+public class Show : AggregateRoot
+{
+    public ShowStatus Status { get; private set; }
+
+    public NumberOfTickets NumberOfTickets { get; set; }
+}

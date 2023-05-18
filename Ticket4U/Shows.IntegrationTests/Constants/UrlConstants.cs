37db3427c10cf329fc33da879983266accaff6b8
DuleDimitrov123 <dusan.dimitrov@elfak.rs>
2023-05-18 18:50:19 +0200

    Implement integration tests for Category

---
+++ Ticket4U/Shows.IntegrationTests/Constants/UrlConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Shows.IntegrationTests.Constants;
+
+public static class UrlConstants
+{
+    #region CategoryConstants
+
+    public const string BaseCategoryURL = "/api/categories";
+
+    public const string SpecificArchiveCategoryURL = "archive";
+
+    #endregion
+}
--- Ticket4U/Shows.IntegrationTests/Controllers/CategoriesControllerTests.cs
+++ Ticket4U/Shows.IntegrationTests/Controllers/CategoriesControllerTests.cs
@@ -1,6 +1,14 @@
-﻿using Shouldly;
+﻿using Azure;
+using EmptyFiles;
+using Shouldly;
+using Shows.Api.Requests.Categories;
 using Shows.Application.Features.Categories.Queries;
+using Shows.Domain.Categories;
 using Shows.IntegrationTests.Base;
+using Shows.IntegrationTests.Constants;
+using System.Net;
+using System.Net.Http.Json;
+using System.Runtime.InteropServices;
 using System.Text.Json;
 
 namespace Shows.IntegrationTests.Controllers;
@@ -8,11 +16,13 @@ namespace Shows.IntegrationTests.Controllers;
 public class CategoriesControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
 {
     private readonly CustomWebApplicationFactory<Program> _factory;
+    private readonly HttpClient _client;
     private readonly JsonSerializerOptions _jsonSerializerOptions;
 
     public CategoriesControllerTests(CustomWebApplicationFactory<Program> factory)
     {
         _factory = factory;
+        _client = _factory.GetAnonymousClient();
 
         _jsonSerializerOptions = new JsonSerializerOptions
         {
@@ -23,11 +33,7 @@ public class CategoriesControllerTests : IClassFixture<CustomWebApplicationFacto
     [Fact]
     public async Task GetCategoriesSuccessResult()
     {
-        var client = _factory.GetAnonymousClient();
-
-        var response = await client.GetAsync("/api/categories");
-
-        response.EnsureSuccessStatusCode();
+        var response = await _client.GetAsync(UrlConstants.BaseCategoryURL);
 
         var responseString = await response.Content.ReadAsStringAsync();
 
@@ -36,5 +42,126 @@ public class CategoriesControllerTests : IClassFixture<CustomWebApplicationFacto
         result.ShouldNotBeNull();
         result.ShouldBeOfType<List<CategoryResponse>>();
         result.Count.ShouldNotBe(0);
+    }
+
+    [Fact]
+    public async Task GetPersonByIdNotFound()
+    {
+        var response = await _client.GetAsync($"{UrlConstants.BaseCategoryURL}/{Guid.NewGuid()}");
+
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task GetPersonWithEmptyGuid()
+    {
+        var response = await _client.GetAsync($"{UrlConstants.BaseCategoryURL}/{Guid.Empty}");
+
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task GetCategorySuccessfully()
+    {
+        //first create category
+        var newCategory = new CreateCategoryRequest("Test Category", "Description of Test Category");
+        var categoryId = await CreateCategory(newCategory);
+
+        categoryId.ShouldNotBe(Guid.Empty);
+
+        //now get already created category
+        var category = await GetCategory(categoryId);
+
+        category!.Name.ShouldBe(newCategory.Name);
+        category!.Description.ShouldBe(newCategory.Description);
+        category!.Status.ShouldBe(CategoryStatus.IsValid.ToString());
+    }
+
+    [Fact]
+    public async Task CreateCategorySuccessfully()
+    {
+        var newCategory = new CreateCategoryRequest("Test Category", "Description of Test Category");
+
+        var categoryId = await CreateCategory(newCategory);
+
+        categoryId.ShouldNotBe(Guid.Empty);
+    }
+
+    [Fact]
+    public async Task UpdateCategorySuccessfully()
+    {
+        //first create category
+        var newCategory = new CreateCategoryRequest("Test Category", "Description of Test Category");
+        var categoryId = await CreateCategory(newCategory);
+
+        categoryId.ShouldNotBe(Guid.Empty);
+
+        //update category
+        var updatedCategory = new UpdateCategoryRequest("Test Category Updated", "Description of Test Category Updated");
+
+        await UpdateCategory(categoryId, updatedCategory);
+
+        //get category and verify updated fields
+        var category = await GetCategory(categoryId);
+
+        category!.Name.ShouldBe(updatedCategory.NewName);
+        category!.Description.ShouldBe(updatedCategory.NewDescription);
+    }
+
+    private async Task UpdateCategory(Guid categoryId, UpdateCategoryRequest updateCategoryRequest)
+    {
+        var response = await _client.PutAsJsonAsync($"{UrlConstants.BaseCategoryURL}/{categoryId}", updateCategoryRequest);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+    }
+
+    [Fact]
+    public async Task ArchiveCategorySuccessfully()
+    {
+        //create category first
+        var newCategory = new CreateCategoryRequest("Test Category", "Description of Test Category");
+
+        var categoryId = await CreateCategory(newCategory);
+
+        categoryId.ShouldNotBe(Guid.Empty);
+
+        //archive
+        await ArchiveCategory(categoryId);
+
+        //get
+        var category = await GetCategory(categoryId);
+
+        category.ShouldNotBeNull();
+
+        category!.Status.ShouldBe(CategoryStatus.IsArchived.ToString());
+    }
+
+    private async Task<Guid> CreateCategory(CreateCategoryRequest createCategoryRequest)
+    {
+        var createResponse = await _client.PostAsJsonAsync($"{UrlConstants.BaseCategoryURL}", createCategoryRequest);
+        var categoryId = await createResponse.Content.ReadFromJsonAsync<Guid>();
+
+        createResponse.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        return categoryId;
+    }
+
+    private async Task<CategoryResponse> GetCategory(Guid categoryId)
+    {
+        var getResponse = await _client.GetAsync($"{UrlConstants.BaseCategoryURL}/{categoryId}");
+        var category = await getResponse.Content.ReadFromJsonAsync<CategoryResponse>();
+
+        getResponse.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        category.ShouldNotBeNull();
+
+        return category;
+    }
+
+    private async Task ArchiveCategory(Guid categoryId)
+    {
+        var response = await _client.PutAsync($"{UrlConstants.BaseCategoryURL}/{categoryId}/{UrlConstants.SpecificArchiveCategoryURL}", null);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
     }
 }

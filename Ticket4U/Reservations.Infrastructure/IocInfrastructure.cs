89ef427a18acb2f5be003e3cd5949dfdb0d4537e
DuleDimitrov123 <dusan.dimitrov@elfak.rs>
2023-06-11 13:47:30 +0200

    Setup reservations clean architecture

---
+++ Ticket4U/Reservations.Infrastructure/IocInfrastructure.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Reservations.Infrastructure;
+
+public static class IocInfrastructure
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        //TODO: Add DbContext and Repos
+
+        return services;
+    }
+}

83c4aff99daf1c541c468946014dbc9ba17f1843
DuleDimitrov123 <dusan.dimitrov@elfak.rs>
2023-06-15 22:34:13 +0200

    Fix integration tests by adding Testcontainers

--- Ticket4U/Shows.IntegrationTests/Base/CustomWebApplicationFactory.cs
+++ Ticket4U/Shows.IntegrationTests/Base/CustomWebApplicationFactory.cs
@@ -4,28 +4,40 @@ using Shows.Infrastructure.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
+using Shows.Application.Contracts.Persistance;
+using Shows.Domain.Shows;
+using Shows.Application.Contracts.Outbox;
+using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Shows.IntegrationTests.Overrides;
 
 namespace Shows.IntegrationTests.Base;
 
 public class CustomWebApplicationFactory<TStartup>
-            : WebApplicationFactory<TStartup> where TStartup : class
+            : WebApplicationFactory<TStartup>, IAsyncLifetime where TStartup : class
 {
+    private readonly MsSqlTestcontainer _container =
+        new TestcontainersBuilder<MsSqlTestcontainer>()
+        .WithDatabase(new MsSqlTestcontainerConfiguration
+        {
+            Database = "ShowsIntegrationTests",
+            Password = "localdevpassword#123",
+        })
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+        .WithCleanUp(true)
+        .Build();
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
         {
-            var descriptor = services.SingleOrDefault
-                   (d => d.ServiceType == typeof(DbContextOptions<ShowsDbContext>));
+            services.AddTestDbContext(_container.ConnectionString);
 
-            if (descriptor != null)
-            {
-                services.Remove(descriptor);
-            }
-
-            services.AddDbContext<ShowsDbContext>(options =>
-            {
-                options.UseInMemoryDatabase("ShowsConnectionStringInMemoryTest");
-            });
+            services.RemoveAll<IShowPublisher>();
+            services.AddScoped<IShowPublisher, TestShowPublisher>();
 
             var sp = services.BuildServiceProvider();
 
@@ -49,8 +61,13 @@ public class CustomWebApplicationFactory<TStartup>
         });
     }
 
+    public async Task InitializeAsync() => await _container.StartAsync();
+
+    public new async Task DisposeAsync() => await _container.DisposeAsync();
+
     public HttpClient GetAnonymousClient()
     {
         return CreateClient();
+
     }
 }

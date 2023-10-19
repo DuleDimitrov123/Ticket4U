496bad702016157ad2f707247b03faf1e853c2ec
DuleDimitrov123 <dusan.dimitrov@elfak.rsa>
2023-10-19 07:34:03 +0200

    add swagger in api gateway POC

--- Ticket4U/Ticket4U.ApiGateway/Program.cs
+++ Ticket4U/Ticket4U.ApiGateway/Program.cs
@@ -1,6 +1,8 @@
+using MMLib.SwaggerForOcelot.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Shared.Infrastructure.Authentication;
+using Ticket4U.ApiGateway;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,13 +10,27 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCustomAuthentication(builder.Configuration);
 
+var routes = "Routes";
+
+builder.Configuration.AddOcelotWithSwaggerSupport(options =>
+{
+    options.Folder = routes;
+});
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);//.AddPoly();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddOcelot(routes, builder.Environment)
+    .AddEnvironmentVariables();
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
@@ -31,6 +47,13 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-await app.UseOcelot();
+//await app.UseOcelot();
+
+app.UseSwaggerForOcelotUI(options =>
+{
+    options.PathToSwaggerGenerator = "/swagger/docs";
+    options.ReConfigureUpstreamSwaggerJson = AlterUpstream.AlterUpstreamSwaggerJson;
+
+}).UseOcelot().Wait();
 
 app.Run();
